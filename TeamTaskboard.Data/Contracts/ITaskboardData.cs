24348bcbf7d517412b0ed9a2c545d252258491dc
lostm1nd <kliomba@abv.bg>
2014-11-04 22:28:10 +0200

    :octopus: Add Repository and UOW

---
+++ TeamTaskboard.Data/Contracts/ITaskboardData.cs
@@ -0,0 +1,11 @@
+﻿namespace TeamTaskboard.Data.Contracts
+{
+    using TeamTaskboard.Models;
+
+    public interface ITaskboardData
+    {
+        IRepository<User> Users { get; }
+
+        int SaveChanges();
+    }
+}
