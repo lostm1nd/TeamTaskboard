d9f2e2fc807c94295d9d360bfd94755b72a54a23
lostm1nd <kliomba@abv.bg>
2014-11-06 20:57:52 +0200

    Add ajax loading for forms in Profile page

---
+++ TeamTaskboard.App/Controllers/ProfileController.cs
@@ -0,0 +1,23 @@
+﻿namespace TeamTaskboard.App.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+
+    public class ProfileController : Controller
+    {
+
+        public ActionResult ChangePassword()
+        {
+            return PartialView("_ChangePasswordPartial");
+        }
+
+        public ActionResult SetPassword()
+        {
+            return PartialView("_SetPasswordPartial");
+        }
+    }
+}
