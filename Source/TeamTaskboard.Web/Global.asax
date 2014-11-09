e08657c64e0c4e2ff8640fb39f9e7e700dfb473a
Deyan Yanakiev <kliomba@abv.bg>
2014-11-09 17:11:52 +0200

    Create project and update all packages

---
+++ Source/TeamTaskboard.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TeamTaskboard.Web.MvcApplication" Language="C#" %>
---
+++ Source/TeamTaskboard.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamTaskboard.Web.Startup))]
+namespace TeamTaskboard.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
