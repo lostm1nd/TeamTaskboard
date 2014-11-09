8c529d5d75958647e5a7a28c24c211ad049bcd7d
Deyan Yanakiev <kliomba@abv.bg>
2014-11-09 18:28:43 +0200

    Add a base controller

---
+++ Source/TeamTaskboard.Web/Controllers/BaseController.cs
@@ -0,0 +1,26 @@
+﻿namespace TeamTaskboard.Web.Controllers
+{
+    using System.Threading;
+    using System.Web.Mvc;
+
+    using Microsoft.AspNet.Identity;
+
+    using TeamTaskboard.Data.Contracts;
+    using TeamTaskboard.Models;
+
+    public abstract class BaseController : Controller
+    {
+        public BaseController(ITaskboardData data)
+        {
+            this.Data = data;
+            this.CurrentUser = data.Users.GetById(Thread.CurrentPrincipal.Identity.GetUserId());
+            this.CurrentTeam = this.CurrentUser.Team;
+        }
+
+        public ITaskboardData Data { get; private set; }
+
+        public TaskboardUser CurrentUser { get; private set; }
+
+        public Team CurrentTeam { get; private set; }
+    }
+}
