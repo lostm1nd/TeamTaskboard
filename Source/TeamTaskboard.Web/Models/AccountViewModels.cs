5880baa3a450dbccc0ef38f9edba1d07e8846652
Deyan Yanakiev <kliomba@abv.bg>
2014-11-11 21:58:28 +0200

    Change the login page to ask for username not email

--- Source/TeamTaskboard.Web/Models/AccountViewModels.cs
+++ Source/TeamTaskboard.Web/Models/AccountViewModels.cs
@@ -49,9 +49,8 @@ namespace TeamTaskboard.Web.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "Name")]
+        public string Name { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
