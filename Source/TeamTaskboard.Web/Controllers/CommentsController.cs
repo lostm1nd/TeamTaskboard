846dc496a4882906713ef317d1df82c330cbcc89
Deyan Yanakiev <kliomba@abv.bg>
2014-11-14 17:40:11 +0200

    Add tinymce for adding comments to tasks

---
+++ Source/TeamTaskboard.Web/Controllers/CommentsController.cs
@@ -0,0 +1,27 @@
+﻿namespace TeamTaskboard.Web.Controllers
+{
+    using System.Web.Mvc;
+    using TeamTaskboard.Data.Contracts;
+
+    [Authorize]
+    public class CommentsController : BaseController
+    {
+        public CommentsController(ITaskboardData data)
+            : base(data)
+        {
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create(int taskId)
+        {
+            ViewBag.TaskId = taskId;
+            return PartialView("_CreateComment");
+        }
+    }
+}
