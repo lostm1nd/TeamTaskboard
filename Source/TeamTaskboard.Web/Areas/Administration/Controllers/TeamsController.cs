bd095b66b5b55b48e314bf1b324ad064a0561ce1
Deyan Yanakiev <kliomba@abv.bg>
2014-11-16 13:48:45 +0200

    Add kendo grid to manage team view in admin area

--- Source/TeamTaskboard.Web/Areas/Administration/Controllers/TeamsController.cs
+++ Source/TeamTaskboard.Web/Areas/Administration/Controllers/TeamsController.cs
@@ -1,7 +1,14 @@
 ﻿namespace TeamTaskboard.Web.Areas.Administration.Controllers
 {
     using System.Web.Mvc;
+
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Kendo.Mvc.UI;
+    using Kendo.Mvc.Extensions;
+
     using TeamTaskboard.Data.Contracts;
+    using TeamTaskboard.Web.Areas.Administration.ViewModels;
 
     public class TeamsController : AdminBaseController
     {
@@ -10,9 +17,21 @@
         {
         }
 
+        [HttpGet]
         public ActionResult Manage()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Read([DataSourceRequest]DataSourceRequest request)
+        {
+            var teams = this.Data.Teams.GetAll()
+                .Project()
+                .To<TeamViewModel>()
+                .ToDataSourceResult(request);
+
+            return Json(teams);
         }
     }
 }
--- Source/TeamTaskboard.Web/Areas/Administration/ViewModels/TeamViewModel.cs
+++ Source/TeamTaskboard.Web/Areas/Administration/ViewModels/TeamViewModel.cs
@@ -1,19 +1,25 @@
 ﻿namespace TeamTaskboard.Web.Areas.Administration.ViewModels
 {
+    using System.ComponentModel.DataAnnotations;
     using System.Linq;
-
     using TeamTaskboard.Models;
     using TeamTaskboard.Web.Infrastructure.Mappings;
     public class TeamViewModel : IMapFrom<Team>, ICustomMappings
     {
+        [Display(Name="Id")]
         public int TeamId { get; set; }
 
+        [Display(Name="Team Name")]
         public string Name { get; set; }
 
+        [Display(Name="Team Description")]
+        [DisplayFormat(NullDisplayText="No description provided.")]
         public string Description { get; set; }
 
+        [Display(Name = "Members Count")]
         public int MembersCount { get; set; }
 
+        [Display(Name = "Projects Count")]
         public int ProjectsCount { get; set; }
 
         public void CreateMappings(AutoMapper.IConfiguration configuration)
