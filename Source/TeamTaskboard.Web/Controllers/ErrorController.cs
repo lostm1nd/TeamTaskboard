df54d496f8a9a9c8993579b2b4feacbaab931eae
Deyan Yanakiev <kliomba@abv.bg>
2014-11-18 08:37:47 +0200

    Add Error contoller and views

---
+++ Source/TeamTaskboard.Web/Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿namespace TeamTaskboard.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class ErrorController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("Error");
+        }
+
+        [HttpGet]
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ServerError()
+        {
+            return View();
+        }
+    }
+}
